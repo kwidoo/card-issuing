afbc34ccc182b82cc1128b6b366a189b0d90b9d3
Oleg Pashkovsky <oleg@pashkovsky.me>
2022-05-19 12:19:39 +0300

    #6 Merchant Model stub

---
+++ src/Models/Merchant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kwidoo\CardIssuing\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merchant extends Model
+{
+    use HasFactory;
+}
